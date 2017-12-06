fe725f8264938058d3f4307576926e659ad0380a
alan <me@alansoto.info>
2017-12-06 23:10:45 +1000

    bug: add margin-top to main App element

--- src/App.js
+++ src/App.js
@@ -3,8 +3,16 @@ import PostList from './post-list'
 
 class App extends Component {
   render() {
+    const style = {
+      mainContainer:{
+        marginTop:'3em'
+      }
+    }
+
     return (
-      <PostList />
+      <div style={style.mainContainer}>
+        <PostList />
+      </div>
     );
   }
 }
